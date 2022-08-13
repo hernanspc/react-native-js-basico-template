96405bb4cc0765229dc14d2775bc0b4cc524c4f5
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-08-12 19:45:37 -0500

    add tweetcard

---
+++ src/components/TweetCard.js
@@ -0,0 +1,55 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+
+const TweetCard = ({
+    id,
+    author,
+    fullText,
+}) => {
+    return (
+        <View style={styles.singleItem}>
+            <View style={styles.row}>
+                <Image
+                    source={{ uri: author.avatar }}
+                    style={styles.avatar}
+                />
+                <View style={styles.tweetContent}>
+                    <Text style={styles.description}>{fullText}</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default TweetCard
+
+const styles = StyleSheet.create({
+    singleItem: {
+        margin: 8,
+        padding: 8,
+        minHeight: 44,
+        flex: 1,
+        borderWidth: 1,
+        borderColor: 'lightgray',
+        borderRadius: 10,
+        backgroundColor: 'ivory'
+    },
+    description: {
+        fontSize: 14,
+        color: 'darkslategray'
+    },
+    avatar: {
+        width: 44,
+        height: 44,
+        borderRadius: 22,
+        marginRight: 16,
+        marginTop: 4,
+    },
+    row: {
+        flexDirection: 'row',
+    },
+    tweetContent: {
+        flexShrink: 1,
+        flexGrow: 1
+    }
+})
