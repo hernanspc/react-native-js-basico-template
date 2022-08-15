3e394b898229e682c1c1af2f9b79a9d41d05f288
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-08-15 09:23:07 -0500

    update

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import Router from './src/router/router'
 
 function HomeScreen({ navigation }) {
   return (
@@ -26,11 +27,12 @@ const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Drawer.Navigator initialRouteName="Home">
+    //     <Drawer.Screen name="Home" component={HomeScreen} />
+    //     <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+    //   </Drawer.Navigator>
+    // </NavigationContainer>
+    <Router />
   );
 }
