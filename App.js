c2b5f5635483f0a8f72d2856845d67a3cde46585
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-08-16 09:09:46 -0500

    NativeBaseProvider

--- App.js
+++ App.js
@@ -4,9 +4,12 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import Router from './src/router/router'
 import MyDrawer from './DrawerPrincipal';
+import { NativeBaseProvider } from "native-base";
 
 export default function App() {
   return (
-    <MyDrawer />
+    <NativeBaseProvider>
+      <MyDrawer />
+    </NativeBaseProvider>
   );
 }
